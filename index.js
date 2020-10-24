be1e8d670bbe94040e4a4a660a2b217931dbcfa5
david11yf29 <david11yf29@gmail.com>
2020-10-24 14:17:28 +0800

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-const canvas = d3.select('.canvas');
-const svg = canvas.append('svg')
-  .attr('height', 600)
-  .attr('weight', 600);
-
-
-svg.append('circle');
-svg.append('rect');
-svg.append('line');
