6ca6dbefc8487204f612c330ad29e610f4b7cb29
david11yf29 <david11yf29@gmail.com>
2020-10-24 23:33:36 +0800

    updated

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const svg = d3.select('svg');
 
+d3.json('./menu.json').then(data => {
+  
+  // join data to rect (only one rect element)
+  const rect = svg.selectAll('rect')
+    .data(data)
+
+  rect.attr('width', 50)
+    .attr('height', d => d.orders)
+    .attr('fill', 'orange')
+    .attr('x', (d,i) => i * 70)
+
+})
