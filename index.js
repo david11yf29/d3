7be38cf72a329559e004ab38346ab02c265ffc81
david11yf29 <david11yf29@gmail.com>
2020-10-27 18:54:38 +0800

    updated

--- index.js
+++ index.js
@@ -85,19 +85,49 @@ const update = (data) => {
 };
 
 
+let data = [];
 // get data from firestore
-db.collection('dishes').get().then(res => {
+db.collection('dishes').onSnapshot(res => {
 
-  const data = [];
-  res.docs.forEach(doc => {
-    data.push(doc.data());
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch(change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+
+    console.log(change);
+    
   });
 
   update(data);
+})
 
-  // d3.interval(() => {
-  //   data.pop()
-  //   update(data);
-  // }, 3000)
 
-});
+
+
+
+
+// // get data from firestore
+// db.collection('dishes').get().then(res => {
+
+//   const data = [];
+//   res.docs.forEach(doc => {
+//     data.push(doc.data());
+//   });
+
+//   update(data);
+
+// });
