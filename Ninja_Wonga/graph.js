72e79c09d63278822370c6f9ead295c8fad68fdf
david11yf29 <david11yf29@gmail.com>
2020-10-29 15:20:55 +0800

    updated

--- Ninja_Wonga/graph.js
+++ Ninja_Wonga/graph.js
@@ -13,14 +13,66 @@ const pie = d3.pie() // return function
   .sort(null)
   .value(d => d.cost);
 
-const angles = pie([
-  { name: 'rent', cost: 500 },
-  { name: 'bills', cost: 300 },
-  { name: 'gaming', cost: 200 }
-]);
+// const angles = pie([
+//   { name: 'rent', cost: 500 },
+//   { name: 'bills', cost: 300 },
+//   { name: 'gaming', cost: 200 }
+// ]);
 
 const arcPath = d3.arc()
   .outerRadius(dims.radius)
-  .innerRadius(dims.radius / 2)
+  .innerRadius(dims.radius / 2);
 
-console.log(arcPath(angles[0]));
+const color = d3.scaleOrdinal(d3['schemeSet3'])
+
+// update function
+const update = (data) => {
+
+  // update color scale domain
+  color.domain(data.map(d => d.name));
+  
+  // join enhanced (pie) data to path elements
+  const paths = graph.selectAll('path')
+    .data(pie(data));
+
+  paths.enter()
+    .append('path')
+      .attr('class', 'arc')
+      .attr('d', arcPath)
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 3)
+      .attr('fill', d => color(d.data.name)) // name is inside pie(data)'s data property
+
+
+
+};
+
+
+// data array and firestore
+let data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+  res.docChanges().forEach(change => {
+
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      case "added":
+        data.push(doc);
+        break;
+      case "modified":
+        const index = data.findIndex(item => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case "removed":
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  })
+
+  update(data);
+
+})
