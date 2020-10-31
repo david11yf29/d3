d11fd956806d6788e7ee991d118eb2d0de9e54e0
david11yf29 <david11yf29@gmail.com>
2020-10-31 16:04:27 +0800

    updated

--- Ninja_Wonga/index.js
+++ Ninja_Wonga/index.js
@@ -1,1 +1,26 @@
 const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const cost = document.querySelector('#cost');
+const error = document.querySelector('#error');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (name.value && cost.value) {
+
+    const item = {
+      name: name.value,
+      cost: parseInt(cost.value)
+    };
+
+    db.collection('expense').add(item).then(res => {
+      name.value = "";
+      cost.value = "";
+      error.textContent = "";
+    })
+
+  } else {
+    error.textContent = "Please enter value before submitting"
+  }
+
+})
